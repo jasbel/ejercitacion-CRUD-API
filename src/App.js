6b09591d333b9f58ea30daa901e0c173e89089a5
jasbel <asbel.elmec@gmail.com>
2022-08-03 08:16:30 -0400

    feat: small changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Edit, Delete } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { Button ,TextField, Modal} from '@material-ui/core';
-
-
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Edit, Delete } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Button, TextField, Modal } from "@material-ui/core";
 
 function App() {
-
-    //url obtenida de mockAPI
-    const url = 'https://60a6e6f9b970910017eb2862.mockapi.io/users/articles/'
+  //url obtenida de mockAPI
+  const url = "https://60a6e6f9b970910017eb2862.mockapi.io/users/articles/";
 
   const [data, setData] = useState([]);
   const estilos = useStyles();
@@ -26,169 +23,192 @@ function App() {
   const [modalEditar, setModalEditar] = useState(false);
   const [modalEliminar, setModalEliminar] = useState(false);
 
-
-
   //obtenemos lo que el usuario ingresa y la captamos por la funcion handlerChange
   const [consolaSeleccionada, setConsolaSeleccionada] = useState({
-    name: '',
-    empresa : '',
-    lanzamiento : '',
-    unidades_vendidas: ''
+    name: "",
+    empresa: "",
+    lanzamiento: "",
+    unidades_vendidas: "",
   });
 
-const handlerChange = (e) =>{
-  const {name, value} = e.target;
-  setConsolaSeleccionada(prevState =>({
-    ...prevState,
-    [name] : value
-  }))
-}
-console.log(consolaSeleccionada);
+  const handlerChange = e => {
+    const { name, value } = e.target;
+    setConsolaSeleccionada(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  console.log(consolaSeleccionada);
 
-//funcion para saber que consola esta seleccionada
+  //funcion para saber que consola esta seleccionada
 
-const seleccionarConsola=(consola, caso)=>{
-  setConsolaSeleccionada(consola);
-  (caso==='editar')? abrirCerrarModalEditar(): abrirCerrarModalEliminar()
-}
-
-
+  const seleccionarConsola = (consola, caso) => {
+    setConsolaSeleccionada(consola);
+    caso === "editar" ? abrirCerrarModalEditar() : abrirCerrarModalEliminar();
+  };
 
   //---------------------------PETICIONES------------------------------
 
   //peticion GET
   const peticionGet = async () => {
-    await axios.get(url)
-      .then(response => {
-        setData(response.data);
-        console.log(data);
-      })
-  }
+    await axios.get(url).then(response => {
+      setData(response.data);
+      console.log({ data });
+    });
+  };
 
   useEffect(async () => {
     await peticionGet();
-  }, [])
-
+  }, []);
 
   //peticion POST
 
-  const peticionPost = async() =>{
-    await axios.post(url, consolaSeleccionada)
-    .then(response =>{
-      setData(data.concat(response.data))
-      abrirCerrarModalInsertar()
-    })
-  }
+  const peticionPost = async () => {
+    await axios.post(url, consolaSeleccionada).then(response => {
+      setData(data.concat(response.data));
+      abrirCerrarModalInsertar();
+    });
+  };
 
   // //peticion PUT
 
-  const peticionPut = async () =>{
-    await axios.put(url + consolaSeleccionada.id, consolaSeleccionada)
-    .then(response=>{
-      let dataNueva= data;
+  const peticionPut = async () => {
+    await axios.put(url + consolaSeleccionada.id, consolaSeleccionada).then(response => {
+      let dataNueva = data;
       console.log(dataNueva);
-      dataNueva.map(consola =>{
-        if(consolaSeleccionada.id === consola.id){
+      dataNueva.map(consola => {
+        if (consolaSeleccionada.id === consola.id) {
           consola.name = consolaSeleccionada.name;
-          consola.empresa= consolaSeleccionada.empresa;
-          consola.lanzamiento=consolaSeleccionada.lanzamiento;
-          consola.unidades_vendidas=consolaSeleccionada.unidades_vendidas;
+          consola.empresa = consolaSeleccionada.empresa;
+          consola.lanzamiento = consolaSeleccionada.lanzamiento;
+          consola.unidades_vendidas = consolaSeleccionada.unidades_vendidas;
         }
-      })
+      });
       setData(dataNueva);
       abrirCerrarModalEditar();
-    })
-  }
+    });
+  };
   //peticion DELETE
 
-  const peticionDelete =async()=>{
-   await axios.delete(url+consolaSeleccionada.id)
-   .then(response =>{
-    setData(data.filter(consola => consola.id!==consolaSeleccionada.id ));
-    abrirCerrarModalEliminar()
-   })
-  }
+  const peticionDelete = async () => {
+    await axios.delete(url + consolaSeleccionada.id).then(response => {
+      setData(data.filter(consola => consola.id !== consolaSeleccionada.id));
+      abrirCerrarModalEliminar();
+    });
+  };
 
   //--------------------------MANEJO MODAL-----------------------------
 
   const abrirCerrarModalInsertar = () => {
-    setModalInsertar(!modalInsertar)
+    setModalInsertar(!modalInsertar);
   };
 
   const abrirCerrarModalEditar = () => {
-    setModalEditar(!modalEditar)
+    setModalEditar(!modalEditar);
   };
 
   const abrirCerrarModalEliminar = () => {
-    setModalEliminar(!modalEliminar)
+    setModalEliminar(!modalEliminar);
   };
-
 
   //Body MODAL
   const bodyInsertar = (
-      <div className={estilos.modal}>
+    <div className={estilos.modal}>
       <h3>Agregar Nueva Consola</h3>
-      <TextField name='name' className={estilos.inputMaterial} label='Nombre' onChange={handlerChange}></TextField>
+      <TextField name="name" className={estilos.inputMaterial} label="Nombre" onChange={handlerChange}></TextField>
       <br />
-      <TextField name='empresa' className={estilos.inputMaterial} label='Empresa' onChange={handlerChange}></TextField>
+      <TextField name="empresa" className={estilos.inputMaterial} label="Empresa" onChange={handlerChange}></TextField>
       <br />
-      <TextField name='lanzamiento' className={estilos.inputMaterial} label='Fecha de Lanzamiento' onChange={handlerChange}></TextField>
+      <TextField name="lanzamiento" className={estilos.inputMaterial} label="Fecha de Lanzamiento" onChange={handlerChange}></TextField>
       <br />
-      <TextField name='unidades_vendidas' className={estilos.inputMaterial} label='Unidades Vendidas' onChange={handlerChange}></TextField>
-      <br />
-      <div align="right">
-        <Button onClick={()=>peticionPost()} color="primary">Insertar</Button>
-        <Button onClick={()=>abrirCerrarModalInsertar()}>Cancelar</Button>
-      </div>
-    </div>
-    )
-  
-    const bodyEditar = (
-      <div className={estilos.modal}>
-      <h3> Editar Consola</h3>
-      <TextField name='name' className={estilos.inputMaterial} label='Nombre' onChange={handlerChange} value={consolaSeleccionada && consolaSeleccionada.name}></TextField>
-      <br />
-      <TextField name='empresa' className={estilos.inputMaterial} label='Empresa' onChange={handlerChange} value={consolaSeleccionada && consolaSeleccionada.empresa}></TextField>
-      <br />
-      <TextField name='lanzamiento' className={estilos.inputMaterial} label='Fecha de Lanzamiento' onChange={handlerChange} value={consolaSeleccionada && consolaSeleccionada.lanzamiento}></TextField>
-      <br />
-      <TextField name='unidades_vendidas' className={estilos.inputMaterial} label='Unidades Vendidas' onChange={handlerChange} value={consolaSeleccionada && consolaSeleccionada.unidades_vendidas}></TextField>
+      <TextField name="unidades_vendidas" className={estilos.inputMaterial} label="Unidades Vendidas" onChange={handlerChange}></TextField>
       <br />
       <div align="right">
-        <Button onClick={()=>peticionPut()} color="primary">Editar</Button>
-        <Button onClick={()=>abrirCerrarModalEditar()}>Cancelar</Button>
+        <Button onClick={() => peticionPost()} color="primary">
+          Insertar
+        </Button>
+        <Button onClick={() => abrirCerrarModalInsertar()}>Cancelar</Button>
       </div>
     </div>
-    )
+  );
 
-    const bodyEliminar = (
-      <div className={estilos.modal}>
-      <h3> Estas seguro de eliminar la consola <b>{consolaSeleccionada && consolaSeleccionada.name}</b></h3>
+  const bodyEditar = (
+    <div className={estilos.modal}>
+      <h3> Editar Consola</h3>
+      <TextField
+        name="name"
+        className={estilos.inputMaterial}
+        label="Nombre"
+        onChange={handlerChange}
+        value={consolaSeleccionada && consolaSeleccionada.name}
+      ></TextField>
+      <br />
+      <TextField
+        name="empresa"
+        className={estilos.inputMaterial}
+        label="Empresa"
+        onChange={handlerChange}
+        value={consolaSeleccionada && consolaSeleccionada.empresa}
+      ></TextField>
+      <br />
+      <TextField
+        name="lanzamiento"
+        className={estilos.inputMaterial}
+        label="Fecha de Lanzamiento"
+        onChange={handlerChange}
+        value={consolaSeleccionada && consolaSeleccionada.lanzamiento}
+      ></TextField>
+      <br />
+      <TextField
+        name="unidades_vendidas"
+        className={estilos.inputMaterial}
+        label="Unidades Vendidas"
+        onChange={handlerChange}
+        value={consolaSeleccionada && consolaSeleccionada.unidades_vendidas}
+      ></TextField>
       <br />
       <div align="right">
-        <Button onClick={()=>peticionDelete()} color="secondary">SI</Button>
-        <Button onClick={()=>abrirCerrarModalEliminar()}>NO</Button>
+        <Button onClick={() => peticionPut()} color="primary">
+          Editar
+        </Button>
+        <Button onClick={() => abrirCerrarModalEditar()}>Cancelar</Button>
       </div>
     </div>
-    )
+  );
+
+  const bodyEliminar = (
+    <div className={estilos.modal}>
+      <h3>
+        {" "}
+        Estas seguro de eliminar la consola <b>{consolaSeleccionada && consolaSeleccionada.name}</b>
+      </h3>
+      <br />
+      <div align="right">
+        <Button onClick={() => peticionDelete()} color="secondary">
+          SI
+        </Button>
+        <Button onClick={() => abrirCerrarModalEliminar()}>NO</Button>
+      </div>
+    </div>
+  );
 
   return (
     <div className="App">
       <br />
-      <Button 
-      variant="contained"
-      size="large"
-        className={estilos.buttonInsertar}
-        onClick={()=>abrirCerrarModalInsertar()}>INSERTAR</Button>
+      <Button variant="contained" size="large" className={estilos.buttonInsertar} onClick={() => abrirCerrarModalInsertar()}>
+        INSERTAR
+      </Button>
       <br />
       <TableContainer component={Paper}>
-        <Table> 
+        <Table>
           <TableHead>
             <TableRow>
-              <StyledTableCell>NOMBRE</StyledTableCell>
-              <StyledTableCell>EMPRESA</StyledTableCell>
-              <StyledTableCell>LANZAMIENTO</StyledTableCell>
-              <StyledTableCell>UNIDADES VENDIDAS (millones)</StyledTableCell>
+              <StyledTableCell>TITULO</StyledTableCell>
+              <StyledTableCell>DESCRIPCION</StyledTableCell>
+              <StyledTableCell>PRECIO</StyledTableCell>
+              <StyledTableCell>IMAGE</StyledTableCell>
+              <StyledTableCell>CATEGORIA</StyledTableCell>
+              <StyledTableCell>STOCK</StyledTableCell>
               <StyledTableCell>Acciones</StyledTableCell>
             </TableRow>
           </TableHead>
@@ -196,18 +216,18 @@ const seleccionarConsola=(consola, caso)=>{
           <TableBody>
             {data.map(consola => (
               <StyledTableRow key={consola.id}>
-                <StyledTableCell>{consola.name}</StyledTableCell>
-                <StyledTableCell>{consola.empresa}</StyledTableCell>
-                <StyledTableCell>{consola.lanzamiento}</StyledTableCell>
-                <StyledTableCell>{consola.unidades_vendidas}</StyledTableCell>
+                <StyledTableCell>{consola.title}</StyledTableCell>
+                <StyledTableCell>{consola.description}</StyledTableCell>
+                <StyledTableCell>{consola.price}</StyledTableCell>
                 <StyledTableCell>
-                  <Edit
-                    onClick={()=>seleccionarConsola(consola, 'editar')} 
-                    className={estilos.iconos} />
+                  <img width={50} height={50} src={consola.pictureUrl} alt='' />
+                </StyledTableCell>
+                <StyledTableCell>{consola.categories}</StyledTableCell>
+                <StyledTableCell>{consola.stock}</StyledTableCell>
+                <StyledTableCell>
+                  <Edit onClick={() => seleccionarConsola(consola, "editar")} className={estilos.iconos} />
                   &nbsp;&nbsp;&nbsp;&nbsp;
-                  <Delete
-                    onClick={()=>seleccionarConsola(consola, 'eliminar')}  
-                    className={estilos.iconos} />
+                  <Delete onClick={() => seleccionarConsola(consola, "eliminar")} className={estilos.iconos} />
                 </StyledTableCell>
               </StyledTableRow>
             ))}
@@ -216,26 +236,17 @@ const seleccionarConsola=(consola, caso)=>{
       </TableContainer>
 
       {/* Modal insertar */}
-      <Modal
-        open={modalInsertar}
-        onClose={abrirCerrarModalInsertar}
-        >
+      <Modal open={modalInsertar} onClose={abrirCerrarModalInsertar}>
         {bodyInsertar}
       </Modal>
 
-        {/* Modal editar */}
-        <Modal
-        open={modalEditar}
-        onClose={abrirCerrarModalEditar}
-        >
+      {/* Modal editar */}
+      <Modal open={modalEditar} onClose={abrirCerrarModalEditar}>
         {bodyEditar}
       </Modal>
 
-       {/* Modal eliminar */}
-       <Modal
-        open={modalEliminar}
-        onClose={abrirCerrarModalEliminar}
-        >
+      {/* Modal eliminar */}
+      <Modal open={modalEliminar} onClose={abrirCerrarModalEliminar}>
         {bodyEliminar}
       </Modal>
     </div>
@@ -253,40 +264,40 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   modal: {
-    position: 'absolute',
+    position: "absolute",
     width: 600,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    display:'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    transform: 'traslate(-50%, -50%)',
-    top: '20%',
-    left: '30%',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    transform: "traslate(-50%, -50%)",
+    top: "20%",
+    left: "30%",
   },
   iconos: {
-    cursor: 'pointer',
+    cursor: "pointer",
   },
   inputMaterial: {
-    width: '100%'
+    width: "100%",
   },
-  buttonInsertar :{
-    color: 'blue',
-    marginBottom: '25px'
-  }
-}))
+  buttonInsertar: {
+    color: "blue",
+    marginBottom: "25px",
+  },
+}));
 export default App;
